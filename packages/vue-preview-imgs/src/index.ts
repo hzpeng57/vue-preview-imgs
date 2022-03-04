94eec77f481367c6c3c35839571b57c2811238b8
hzpeng57 <hrrmzp@163.com>
2022-03-04 14:19:12 +0800

    feat: 升级以支持 vue3 版本

--- packages/vue-preview-imgs/src/index.ts
+++ packages/vue-preview-imgs/src/index.ts
@@ -1,64 +1,78 @@
-import Vue, { VueConstructor } from 'vue';
-const PhotoSwipe = require('photoswipe');
-const PhotoSwipeUIDefault = require('photoswipe/dist/photoswipe-ui-default');
-import * as PhotoSwipeUIDefaultType from 'photoswipe/dist/photoswipe-ui-default';
-import * as PhotoSwipeType from 'photoswipe';
+import { App, createApp, defineComponent, h, onMounted, onUnmounted, reactive } from 'vue';
+import PhotoSwipe, { Options, Item } from 'photoswipe';
+import PhotoSwipeUIDefault from 'photoswipe/dist/photoswipe-ui-default';
+import { Options as UIOptions } from 'photoswipe/dist/photoswipe-ui-default';
 import 'photoswipe/dist/default-skin/default-skin.css';
 import 'photoswipe/dist/photoswipe.css';
-import App from './app.vue';
+import Comp from './app.vue';
 
 type ImageItem = {
   alt?: string;
   title?: string;
   el?: Element;
-} & PhotoSwipeType.Item;
+} & Item;
 
 type ShowPhotoOptions = {
   images: Array<ImageItem>;
   usePosition?: boolean;
-} & PhotoSwipeUIDefaultType.Options;
+} & UIOptions;
 
-let vm;
-let gallery: PhotoSwipeType<PhotoSwipeType.Options>;
+let gallery: PhotoSwipe<Options>;
 
 export const showPhoto = (config: ShowPhotoOptions) => {
-  vm = new (Vue.extend(App))({
-    data: () => ({
-      usePosition: false,
-      index: 0,
-      ...config,
-    }),
-    mounted() {
-      document.body.appendChild(this.$el);
-      this.init();
-    },
-    beforeDestroy() {
-      document.body.removeChild(this.$el);
-    },
-    methods: {
-      init() {
-        const pswpElement = document.querySelectorAll('.pswp')[0] as HTMLElement;
-        const options: PhotoSwipeUIDefaultType.Options = {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+  const vm = createApp(
+    defineComponent({
+      setup() {
+        const compOptions = reactive({
+          usePosition: false,
+          index: 0,
           ...config,
-        };
-        if (this.usePosition) {
-          options.getThumbBoundsFn = (index: number) => {
-            const thumbnail = (this.images[index] as ImageItem).el;
-            const pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
-            const rect = (thumbnail as Element).getBoundingClientRect();
-            return { x: rect.left, y: rect.top + pageYScroll, w: rect.width };
+        });
+        const init = () => {
+          const pswpElement = document.querySelectorAll('.pswp')[0] as HTMLElement;
+          const options: UIOptions = {
+            ...config,
           };
-        }
-        gallery = new PhotoSwipe(pswpElement, PhotoSwipeUIDefault, this.images, options);
-        gallery.init();
+          if (compOptions.usePosition) {
+            options.getThumbBoundsFn = (index: number) => {
+              const thumbnail = (compOptions.images[index] as ImageItem).el;
+              const pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
+              const rect = (thumbnail as Element).getBoundingClientRect();
+              return { x: rect.left, y: rect.top + pageYScroll, w: rect.width };
+            };
+          }
+          gallery = new PhotoSwipe(pswpElement, PhotoSwipeUIDefault, compOptions.images, options);
+          gallery.init();
+          gallery.listen('close', () => {
+            setTimeout(() => {
+              // gallery.destroy();
+              vm.unmount();
+              document.body.removeChild(container);
+            }, 500);
+          });
+        };
+        onMounted(() => {
+          init();
+        });
+        onUnmounted(() => {
+          document.body.removeChild(container);
+        });
+        return {
+          ...compOptions,
+        };
       },
-    },
-  });
-  vm.$mount();
+      render() {
+        return h(Comp);
+      },
+    }),
+  );
+  vm.mount(container);
 };
 
 export default {
-  install(Vue: VueConstructor) {
-    Vue.prototype.$previewImg = showPhoto;
+  install(app: App) {
+    app.config.globalProperties.$previewImg = showPhoto;
   },
 };
